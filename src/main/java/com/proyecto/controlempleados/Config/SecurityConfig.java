e66cd089561b3cc7c88095a95d9f855fa8430fd8
Valery Ching <valeryching@hotmail.com>
2026-03-08 00:24:17 -0600

    problemas con los enlaces y correcciones

--- src/main/java/com/proyecto/controlempleados/Config/SecurityConfig.java
+++ src/main/java/com/proyecto/controlempleados/Config/SecurityConfig.java
@@ -25,7 +25,7 @@ public class SecurityConfig {
             )
             .formLogin(login -> login
                     .loginPage("/login")
-                    .defaultSuccessUrl("/",true)
+                    .defaultSuccessUrl("/home", true)
                     .permitAll()
             )
             .logout(logout -> logout
---
+++ src/main/java/com/proyecto/controlempleados/service/CustomUserDetailsService.java
@@ -0,0 +1,33 @@
+package com.proyecto.controlempleados.service;
+
+import com.proyecto.controlempleados.model.Usuario;
+import com.proyecto.controlempleados.repository.UsuarioRepository;
+
+import org.springframework.security.core.userdetails.*;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UsuarioRepository usuarioRepository;
+
+    public CustomUserDetailsService(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        Usuario usuario = usuarioRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuario no encontrado"));
+
+        return new User(
+                usuario.getUsername(),
+                usuario.getPassword(),
+                List.of(new SimpleGrantedAuthority("ROLE_" + usuario.getRol().name()))
+        );
+    }
+}
